d66d138e6780325718df6843e40c675a14d23b0e
OkashaAbdalla <okashaabdalla44@gmail.com>
2026-02-16 08:32:37 +0100

    mfeat: add SkillList component
    - Create container for skill badges
    - Include React, HTML/CSS, JavaScript, and Blockchain skills
    - Pass props to individual Skill components

---
+++ src/components/SkillList.js
@@ -0,0 +1,16 @@
+import Skill from './Skill';
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Graphic Designer" emoji="👶" color="#FF3E00" />
+
+      <Skill skill="HTML+CSS" emoji="👍" color="#E34F26" />
+      <Skill skill="JavaScript" emoji="✨" color="#F7DF1E" />
+      <Skill skill="React" emoji="💪" color="#61DAFB" />
+
+    </div>
+  );
+}
+
+export default SkillList;
